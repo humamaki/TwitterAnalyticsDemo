430c13dbb881b9dbc5d1a1c67cae4497d4fb0cba
Humam Al-Faiz <humam.alfaiz@gmail.com>
2017-01-04 23:02:03 -0500

    Add project files.

---
+++ TwitterAnalytics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwitterAnalytics.MvcApplication" Language="C#" %>
---
+++ TwitterAnalytics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwitterAnalytics.Startup))]
+namespace TwitterAnalytics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
